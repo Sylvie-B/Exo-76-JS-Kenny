c02d32a9219fa20d9f90d19d591796cb41516c2f
Sylvie-B <unsolo@hotmail.fr>
2020-12-06 19:08:24 +0100

    kenny bouge dans chaque direction

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ document.getElementById('up').addEventListener('click',function() {
     t = t - 10;
     document.getElementById('kenny').style.top = t + 'px';
 });
+
+document.getElementById('left').addEventListener("click",function() {
+    let l = parseInt(document.getElementById('kenny').style.left);
+    l = l - 10;
+    document.getElementById('kenny').style.left = l + 'px';
+});
+
+document.getElementById('right').addEventListener("click",function() {
+    let r = parseInt(document.getElementById('kenny').style.left);
+    r = r + 10;
+    document.getElementById('kenny').style.left = r + 'px';
+});
+
+document.getElementById('down').addEventListener("click",function() {
+    let d = parseInt(document.getElementById('kenny').style.top);
+    d = d + 10;
+    document.getElementById('kenny').style.top = d + 'px';
+});
